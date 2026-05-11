37d94fe7b294ebf196373a203ce6046a8135a594
Pranay3010 <pranayrathore30@gmail.com>
2026-05-11 16:16:47 +0530

    memory

--- Excepion_handlin/custom.py
+++ Excepion_handlin/custom.py
@@ -1,13 +1,66 @@
-class NegativeNumberError(Exception):
-    pass
+# class NegativeNumberError(Exception):
+#     pass
 
-num = int(input("Enter Number: "))
+# num = int(input("Enter Number: "))
+
+# try:
+#     if num < 0:
+#         raise NegativeNumberError("Negative number not allowed")
+
+#     print("You entered:", num)
+
+# except NegativeNumberError as e:
+#     print("Error:", e)
+
+
+
+# class InvalidAgeError(Exception):
+#     pass
+
+# age = int(input("Enter age: "))
+
+# try:
+#     if age < 18:
+#         raise InvalidAgeError("Age must be 18 or above")
+
+#     print("Eligible")
+
+# except InvalidAgeError as e:
+#     print(e)
+
+
+# class InsufficientBalanceError(Exception):
+#     pass
+
+# balance = 5000
+# withdraw = int(input("Enter amount: "))
+
+# try:
+#     if withdraw > balance:
+#         raise InsufficientBalanceError("Insufficient balance")
+
+#     balance -= withdraw
+#     print("Remaining balance:", balance)
+
+# except InsufficientBalanceError as e:
+#     print(e)
+
+class MarksError(Exception):
+
+    def __init__(self, marks):
+        self.marks = marks
+
+    def __str__(self):
+        return f"Invalid marks: {self.marks}"
+        
+
+marks = int(input("Enter marks: "))
 
 try:
-    if num < 0:
-        raise NegativeNumberError("Negative number not allowed")
+    if marks > 100:
+        raise MarksError(marks)
 
-    print("You entered:", num)
+    print("Valid Marks")
 
-except NegativeNumberError as e:
-    print("Error:", e)
+except MarksError as e:
+    print(e)
---
+++ memory_mangement/1.py
@@ -0,0 +1,17 @@
+x=10
+
+print(id(x))  # address of 10:
+
+#  there are stack for storage that stores local variable + reference variable
+# heap stores 10;
+
+
+a=[1,2]
+b=[1,2]
+
+print(a==b)
+print(a is b)
+
+c=20
+d=20
+print(c is d)
